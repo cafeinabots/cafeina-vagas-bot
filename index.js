20ce939de1553684e65b2f39718ed606764934bc
biancapereira <biibis@live.com>
2022-01-12 17:33:20 -0300

    Adding welcome message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bot = new TeleBot(process.env.BOT_TOKEN);
 
 // On every text message
 bot.on('text', msg => {
+    let text = msg.text;
     let fromId = msg.from.id;
     let messageId = msg.message_id;
     let answer = `Oi, recebi sua mensagem! Obrigada por enviar uma vaga e/ou material para o nosso canal!
@@ -11,9 +12,22 @@ Nossos ADMs humanos irão avaliar o link e, caso seja bacana para o grupo, o lin
 
 PS: Lembrando que só postamos vagas para iniciantes sem experiência.`;
 
-    bot.forwardMessage(-1001505347688, fromId, messageId);
+    let welcomeMessage = `Oi, eu sou a bot que recebe vagas e outros links para o @CafeinaVagas.
 
-    return bot.sendMessage(fromId, answer);
+❤ Fico no aguardo de novas vagas ou cursos.
+
+CHAT | Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasChat
+
+SAC | Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`
+
+    console.log('User message information', msg);
+
+    if (text === '/start') {
+        return bot.sendMessage(fromId, welcomeMessage);
+    } else {
+        bot.forwardMessage(-1001505347688, fromId, messageId);
+        return bot.sendMessage(fromId, answer);
+    }
 });
 
 bot.connect();
