8118a3af1ae0ebeb7ba8097d11ff51db249527ae
biancapereira <biibis@live.com>
2022-01-12 18:35:37 -0300

    Add foward event

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const TeleBot = require("telebot");
 const bot = new TeleBot(process.env.BOT_TOKEN);
 
-// On every text message
-bot.on("text", (msg) => {
-  let text = msg.text;
-  let fromId = msg.from.id;
-  let messageId = msg.message_id;
-  let answer = `Oi, recebi sua mensagem! Obrigada por enviar uma vaga e/ou material para o nosso canal!
+const answer = `Oi, recebi sua mensagem! Obrigada por enviar uma vaga e/ou material para o nosso canal!
 
 Nossos ADMs humanos irão avaliar o link e, caso seja bacana para o grupo, o link será postado.
 
 PS: Lembrando que só postamos vagas para iniciantes sem experiência.`;
 
-  let welcomeMessage = `Oi, eu sou a bot que recebe vagas e outros links para o @CafeinaVagas.
+const welcomeMessage = `Oi, eu sou a bot que recebe vagas e outros links para o @CafeinaVagas.
 
 ❤ Fico no aguardo de novas vagas, cursos ou outros materiais legais para compartilhar.
 
@@ -20,13 +15,20 @@ CHAT | Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasCh
 
 SAC | Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`;
 
-    let helpMessage = `Nosso bot recebe vagas, cursos e outros tipos de materiais. Basta enviar aqui em formato de texto ou link.
+const helpMessage = `Nosso bot recebe vagas, cursos e outros tipos de materiais. Basta enviar aqui em formato de texto ou link.
 
 ❤ Fico no aguardo de novas vagas, cursos ou outros materiais legais para compartilhar.
 
 CHAT | Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasChat
 
 SAC | Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`;
+
+
+// On every text message
+bot.on(["text", "foward"], (msg) => {
+  let text = msg.text;
+  let fromId = msg.from.id;
+  let messageId = msg.message_id;
 
   console.log("User message information", msg);
 
