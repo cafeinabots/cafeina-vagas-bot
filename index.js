eee81221b7d7d82330ef37d6def8347e2294869c
biancapereira <biibis@live.com>
2022-01-14 19:53:51 -0300

    Otimizando arquivos e permitindo envio de fotos

--- index.js
+++ index.js
@@ -1,43 +1,25 @@
-const TeleBot = require("telebot");
-const bot = new TeleBot(process.env.BOT_TOKEN);
+import { basicAnswer, welcomeMessage, helpMessage } from "./messages.js";
+import Telebot from "telebot";
 
-const answer = `Oi, recebi sua mensagem! Obrigada por enviar uma vaga e/ou material para o nosso canal!
+const bot = new Telebot(process.env.BOT_TOKEN);
 
-Nossos ADMs humanos irão avaliar o link e, caso seja bacana para o grupo, o link será postado.
-
-PS: Lembrando que só postamos vagas para iniciantes sem experiência.`;
-
-const welcomeMessage = `Oi, eu sou a bot que recebe vagas e outros links para o @CafeinaVagas.
-
-❤ Fico no aguardo de novas vagas, cursos ou outros materiais legais para compartilhar.
-
-CHAT | Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasChat
-
-SAC | Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`;
-
-const helpMessage = `Nosso bot recebe vagas, cursos e outros tipos de materiais. Basta enviar aqui em formato de texto ou link.
-
-❤ Fico no aguardo de novas vagas, cursos ou outros materiais legais para compartilhar.
-
-CHAT | Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasChat
-
-SAC | Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`;
+const CHAT_ID = -1001505347688;
 
 // On every text message
-bot.on(["text", "forward"], (msg) => {
+bot.on(["text", "forward", "photo"], (msg) => {
   let text = msg.text;
   let fromId = msg.from.id;
   let messageId = msg.message_id;
 
-  console.log("User message information", msg);
+  console.table("User message information", msg);
 
   if (text === "/start") {
     return bot.sendMessage(fromId, welcomeMessage);
   } else if (text === "/help") {
     return bot.sendMessage(fromId, helpMessage);
   } else {
-    bot.forwardMessage(-1001505347688, fromId, messageId);
-    return bot.sendMessage(fromId, answer);
+    bot.forwardMessage(CHAT_ID, fromId, messageId);
+    return bot.sendMessage(fromId, basicAnswer);
   }
 });
 
---
+++ messages.js
@@ -0,0 +1,21 @@
+export const welcomeMessage = `Oi, eu sou a bot que recebe vagas e outros links para o @CafeinaVagas.
+
+❤ Fico no aguardo de novas vagas, cursos ou outros materiais legais para compartilhar.
+
+💬 Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasChat
+
+🎧 Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`;
+
+export const basicAnswer = `Oi, recebi sua mensagem! Obrigada por enviar uma vaga ou um conteúdo para o nosso canal!
+
+Nossos ADMs humanos irão avaliar o link e, caso seja bacana para o grupo, o link será postado.
+
+PS: Lembrando que só postamos vagas para #iniciantes sem experiência, combinado?`;
+
+export const helpMessage = `Nosso bot recebe vagas, cursos e outros tipos de materiais. Basta enviar aqui em formato de texto, link ou imagens.
+
+❤ Fico no aguardo de novas vagas, cursos ou outros materiais legais para compartilhar.
+
+💬 Conheça o Cafeína Help, nosso chat para tirar dúvidas: @CafeinaVagasChat
+
+🎧 Se você deseja conversar com um ADM, entre em contato pela nossa Central de Atendimento: @SAC_CafeinaVagas`;
