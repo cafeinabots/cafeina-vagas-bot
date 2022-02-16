174fe0bf72460542cc6a43a652926c52908188bf
biancapereira <biibis@live.com>
2022-02-15 21:56:09 -0300

    Testando o objeto de foto

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ bot.on(["text"], (msg) => {
 });
 
 bot.on(["photo"], (msg) => {
-  let photo = msg.photo.file_id;
+  let photo = msg.photo;
   console.log('photoo', photo)
   let fromId = msg.from.id;
   let messageId = msg.message_id;
